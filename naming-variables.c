730cf18428ab6626e65248836417878874757c77
juanurias686-sketch <juanurias686@gmail.com>
2026-04-21 21:55:56 +0000

    recipe and variables

---
+++ naming-variables.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+int number;//signs removed
+int testvalue;//spaces removed
+int example1; // cant start with number
+float int_zzz; // needs a different number
+printf("Variables created successfully.\n");
+}
