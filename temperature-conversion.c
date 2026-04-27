1a2035c528af9d7db5055510e6a5a175d1d81058
juanurias686-sketch <juanurias686@gmail.com>
2026-04-27 14:59:52 +0000

    tfduouhvoih

---
+++ temperature-conversion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+int F;
+int C;
+
+printf("Farenheit: ");
+ scanf("%d", &F);
+
+
+
+ int celcius = F-32/1.8;
+
+ printf("celcius = %d \n" , celcius);
+
+ printf()
+}
