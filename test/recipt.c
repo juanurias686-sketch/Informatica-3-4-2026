361000d64309f0f89c8694de60aabc6efeec280d
juanurias686-sketch <juanurias686@gmail.com>
2026-04-29 17:27:54 +0000

    test

---
+++ test/recipt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void){
+
+int Item;
+int Price;
+int Quantity;
+int Total;
+
+//items
+
+
+float Soda;
+
+
+Soda  = 6.70;
+
+  printf("Black Market Recipt \n");
+    printf("---------------------- \n");
+
+    printf("Enter item: ");
+    scanf("%f", &Soda);
+
+     printf("Enter Price: ");
+    scanf("%d", &Price);
+
+    Price = Soda+ Quantity;
+    Total = (Quantity + Soda);
+     printf("total: %d  all: %d\n", Quantity, Price);
+
+}
+
