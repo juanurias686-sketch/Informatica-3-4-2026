f298d8638410ea9080b737418e6db0fcba055494
juanurias686-sketch <juanurias686@gmail.com>
2026-05-07 19:25:36 +0000

    minutessss

--- week2/minutes.c
+++ week2/minutes.c
@@ -10,7 +10,7 @@ scanf("%d" , &runningTime);
 int startHour;
 int startMin;
 printf("Start time: ");
-scanf("%d %d" , &startHour, &startMin);
+scanf("%d:%d" , &startHour, &startMin);
 
 
 
@@ -19,6 +19,15 @@ int minutes = runningTime % 60;
 
 int endHour = startHour + hours;
 int endMin = startMin + minutes;
+
+
+if(endMin >= 60){
+    endHour = endHour + (endMin / 60);//update endhour variable
+
+    endMin = endMin % 60; //the extra minutes will be rounded with this
+    
+
+}
 
 printf("The movie will last %dhr and %dmin \n" , hours, minutes);
 
